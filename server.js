9220cec8f108286160e17fe4974989075e0c9604
prateek951 <prince.cenation@gmail.com>
2018-04-17 09:27:38 +0530

    middleware and server instantiation

--- server.js
+++ server.js
@@ -5,3 +5,14 @@ const bodyParser = require('body-parser');
 const async = require('async');
 const request = require('request');
 const morgan = require('morgan');
+
+/*@desc(Init app)*/
+const app = express();
+
+/*@desc(Add the required modules)*/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended :  false}));
+
+/*@desc(Make the incoming request hit the server at random port)*/
+const port = process.env.PORT || Math.floor(Math.random()*10000) + 1;
+app.listen(port,() => console.log(`Server running on port: ${port}`));
